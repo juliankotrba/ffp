85a446de4cc84ffa3a9cc7f1ddc6b217b8f43c3c
Julian Kotrba <me@juliankotrba.xyz>
2018-05-21 17:10:45 +0200

    Add part 2/3 of exercise 5

--- exercise-5/AufgabeFFP5.hs
+++ exercise-5/AufgabeFFP5.hs
@@ -3,7 +3,10 @@
 
 module AufgabeFFP5 where
 
---import Data.Char
+import Data.Char
+import Control.Monad
+
+-- Part 1
 
 type Parse0 a b = [a] -> [(b,[a])]
 
@@ -61,9 +64,8 @@ spot p (x:xs)
 dig :: Parse0 Char Char
 dig = spot isDigit
 
-isDigit :: Char -> Bool
-isDigit ch = ('0'<=ch) && (ch<='9')
-
+--isDigit :: Char -> Bool
+--isDigit ch = ('0'<=ch) && (ch<='9')
 
 -- Parser combinators
 
@@ -72,8 +74,8 @@ isDigit ch = ('0'<=ch) && (ch<='9')
 alt :: Parse0 a b -> Parse0 a b -> Parse0 a b
 alt p1 p2 inp = p1 inp ++ p2 inp
 
--- (>*>): combine parsers sequentially 
--- eg. operator: bracket -> number 
+-- (>*>): combine parsers sequentially
+-- eg. operator: bracket -> number
 infixr 5 >*>
 (>*>) :: Parse0 a b -> Parse0 a c -> Parse0 a (b,c)
 (>*>) p1 p2 inp
@@ -90,11 +92,11 @@ build p f inp = [(f x, rem)|(x,rem) <- p inp]
 
 wsParse :: Parse0 Char [Char]
 wsParse (' ':xs) = [([], xs)]
-wsParse a  = [([], a)] 
+wsParse a  = [([], a)]
 
 -- Parsing varibale names (a .. z)
 nameParse :: Parse0 Char Expr
-nameParse = spot (\c -> 'a' <= c && c <= 'z') `build` Var 
+nameParse = spot (\c -> 'a' <= c && c <= 'z') `build` Var
 
 -- Parsing literals (numerals)
 litParse :: Parse0 Char Expr
@@ -109,7 +111,7 @@ optExpParse
 	spot isOp >*>
 	parser >*>
 	token ')' )
-	`build` makeExpr 
+	`build` makeExpr
 
 makeExpr (_,(e1,(op,(e2,_)))) = Op (charToOp op) e1 e2
 
@@ -136,34 +138,174 @@ neList p = (p `build` (:[]))
 	`alt` ((p >*> neList p) `build` (uncurry (:)))
 
 charListToExpr :: [Char] -> Expr
-charListToExpr = Lit . charListToInt 
+charListToExpr = Lit . charListToInt
 
 charListToInt :: [Char] -> Int
-charListToInt ('~':rest) = - (sToInt rest)
-charListToInt other = sToInt other
+charListToInt ('~':rest) = - (read rest)
+charListToInt other = read other
 
-sToInt :: String -> Int
-sToInt = read 
-
-
--- part 2 
+-- Part 2
 
 newtype Parser a = Parse (String -> [(a,String)])
 
 instance Monad Parser where
-p >>= f = Parse (\cs -> concat [(parse (f a)) cs' |
-          (a,cs') <- (parse p) cs])
+	p >>= f = Parse (\cs -> concat [(parse (f a)) cs' |
+        	(a,cs') <- (parse p) cs])
 
-return a = Parse (\cs -> [(a,cs)])
-	where
+	return a = Parse (\cs -> [(a,cs)])
+
+instance MonadPlus Parser where
+	mzero = Parse (\cs -> [])
+	p `mplus` q = Parse (\cs -> parse p cs ++ parse q cs)
+
 parse :: (Parser a) -> (String -> [(a,String)])
 parse (Parse p) = p
 
-
 item :: Parser Char
 item = Parse (\cs -> case cs of
-			"" ->[] 
+			"" ->[]
 			(c:cs) -> [(c,cs)])
 
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = Parse (\cs -> case parse (p `mplus` q) cs of
+	[] -> []
+	(x:xs) -> [x])
 
+char :: Char -> Parser Char
+char c = sat (c ==)
 
+space :: Parser String
+space = many (sat (\c -> c == ' '))
+
+token2 :: Parser a -> Parser a
+token2 p = do {a <- p; space; return a}
+
+sat  :: (Char -> Bool) -> Parser Char
+sat p = do {c <- item; if p c then return c else mzero}
+
+string :: String -> Parser String
+string "" = return ""
+string (c:cs) = do {char c; string cs; return (c:cs)}
+
+many :: Parser a -> Parser [a]
+many p = many1 p +++ return []
+
+many1 :: Parser a -> Parser [a]
+many1 p = do a <- p; as <- many p; return (a:as)
+
+symb :: String -> Parser String
+symb cs = token2 (string cs)
+
+apply  :: Parser a -> String -> [(a,String)]
+apply p = parse (do {space; p})
+
+expr :: Parser Int
+expr = term `chainl1` addop
+
+term = factor `chainl1` mulop
+
+factor = digit +++ do {symb "("; n <- expr; symb ")"; return n}
+
+addop :: Parser (Int -> Int -> Int)
+addop = do {symb "+"; return (+)} +++ do {symb "-"; return (-)}
+
+mulop :: Parser (Int -> Int -> Int)
+mulop = do {symb "*"; return (*)} +++ do {symb "/"; return (div)}
+
+digit = do {x <- token2 (sat isDigit); return (ord x - ord '0')}
+
+chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
+chainl p op a = (p `chainl1` op) +++ return a
+
+chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
+p `chainl1` op = do
+  a <- p
+  rest a
+  where
+    rest a = (do
+      f <- op
+      b <- p
+      rest (f a b)) +++ return a
+
+-- Part 3
+
+newtype Parser2 a b = Parse2 ([a] -> [(b,[a])])
+
+instance Monad (Parser2 a) where
+	p >>= f = Parse2 (\cs -> concat [(parse2 (f a)) cs' |
+        	(a,cs') <- (parse2 p) cs])
+
+	return a = Parse2 (\cs -> [(a,cs)])
+
+instance MonadPlus (Parser2 a) where
+	mzero = Parse2 (\cs -> [])
+	p `mplus` q = Parse2 (\cs -> parse2 p cs ++ parse2 q cs)
+
+parse2 :: (Parser2 a b) -> ([a] -> [(b,[a])])
+parse2 (Parse2 p) = p
+
+item2 :: Parser2 a a
+item2 = Parse2 (\cs -> case cs of
+			[] ->[]
+			(c:cs) -> [(c,cs)])
+
+(++++) :: Parser2 a b -> Parser2 a b -> Parser2 a b
+p ++++ q = Parse2 (\cs -> case parse2 (p `mplus` q) cs of
+	[] -> []
+	(x:xs) -> [x])
+
+char2 :: Char -> Parser2 Char Char
+char2 c = sat2 (c ==)
+
+space2 :: Parser2 Char String
+space2 = many2 (sat2 (\c -> c == ' '))
+
+token3 :: Parser2 Char b -> Parser2 Char b
+token3 p = do {a <- p; space2; return a}
+
+sat2  :: (a -> Bool) -> Parser2 a a
+sat2 p = do {c <- item2; if p c then return c else mzero}
+
+string2 :: String -> Parser2 Char String
+string2 "" = return ""
+string2 (c:cs) = do {char2 c; string2 cs; return (c:cs)}
+
+many2 :: Parser2 a b -> Parser2 a [b]
+many2 p = many12 p ++++ return []
+
+many12 :: Parser2 a b -> Parser2 a [b]
+many12 p = do a <- p; as <- many2 p; return (a:as)
+
+symb2 :: String -> Parser2 Char String
+symb2 cs = token3 (string2 cs)
+
+apply2  :: Parser2 Char b -> String -> [(b,String)]
+apply2 p = parse2 (do {space2; p})
+
+expr2 :: Parser2 Char Int
+expr2 = term2 `chainl12` addop2
+
+term2 = factor2 `chainl12` mulop2
+
+factor2 = digit2 ++++ do {symb2 "("; n <- expr2; symb2 ")"; return n}
+
+addop2 :: Parser2 Char (Int -> Int -> Int)
+addop2 = do {symb2 "+"; return (+)} ++++ do {symb2 "-"; return (-)}
+
+mulop2 :: Parser2 Char (Int -> Int -> Int)
+mulop2 = do {symb2 "*"; return (*)} ++++ do {symb2 "/"; return (div)}
+
+digit2 = do {x <- token3 (sat2 isDigit); return (ord x - ord '0')}
+
+chainl2 :: Parser2 a b -> Parser2 a (b -> b -> b) -> b -> Parser2 a b
+chainl2 p op a = (p `chainl12` op) ++++ return a
+
+chainl12 :: Parser2 a b -> Parser2 a (b -> b -> b) -> Parser2 a b
+p `chainl12` op = do
+  a <- p
+  rest a
+  where
+    rest a = (do
+      f <- op
+      b <- p
+      rest (f a b)) ++++ return a
